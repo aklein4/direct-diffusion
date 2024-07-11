be8dbdfcf95c3fb4a660b5fa1d2885f878974471
Adam Klein <adam3207@gmail.com>
2024-07-10 21:16:12 -0500

    il exposure

--- src/utils/diffusion_utils.py
+++ src/utils/diffusion_utils.py
@@ -175,8 +175,6 @@ def step_to(
     # 6. compute "direction pointing to x_t" of formula (12) from https://arxiv.org/pdf/2010.02502.pdf
     pred_sample_direction = (1 - alpha_prod_t_prev - 0.0**2) ** (0.5) * pred_epsilon
 
-    return pred_sample_direction
-
     # 7. compute x_t without "random noise" of formula (12) from https://arxiv.org/pdf/2010.02502.pdf
     prev_sample = alpha_prod_t_prev ** (0.5) * pred_original_sample + pred_sample_direction
 
